b525903260206e28998128c75c9608494ec73b40
Juan <jschaper@hotmail.co.za>
2023-02-06 13:01:17 +0200

    Redux & Redux Toolkit - basic setup

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
 
 import HomeScreen from './screens/HomeScreen';
+import { store } from './store/redux/store';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name='Home' component={HomeScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
+		<Provider>
+			<NavigationContainer store={store}>
+				<Stack.Navigator>
+					<Stack.Screen name='Home' component={HomeScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</Provider>
 	);
 }
